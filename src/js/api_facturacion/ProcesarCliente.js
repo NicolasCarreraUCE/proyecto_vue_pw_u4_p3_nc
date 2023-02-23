808e62b969856669a4e571cd366522d73a968035
GeoWillC <nacarrera@uce.edu.ec>
2023-02-22 19:47:08 -0500

    taller 33

---
+++ src/js/api_facturacion/ProcesarCliente.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+// Metodos Fachada
+
+// Metodos de consumo CRUD
+const obtenerClientePorCedula = (cedula) => {
+
+}
+
+const insertar = (body) => {
+
+}
+
+const actualizar = (cedula, body) => {
+
+}
