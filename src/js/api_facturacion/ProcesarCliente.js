5495b5025e0d556a2793fd7a4f2e9362569fb744
GeoWillC <nacarrera@uce.edu.ec>
2023-02-23 19:48:23 -0500

    taller 34

--- src/js/api_facturacion/ProcesarCliente.js
+++ src/js/api_facturacion/ProcesarCliente.js
@@ -1,15 +1,55 @@
 import axios from "axios";
 // Metodos Fachada
+export const obtenerPorCedulaFachada = async (cedula) => {
+    return obtenerPorCedula(cedula);
+}
+
+export const insertarFachada = async (body) => {
+    insertar(body)
+}
+
+export const actualizarFachada = (id, body) => {
+    actualizar(id, body)
+}
+
+export const buscarTodosFachada = () => {
+    return buscarTodos()
+}
+
+export const borrarPorIdFachada = (id) => {
+    borrarPorId(id)
+}
+
+export const borrarPorCedulaFachada = (cedula) => {
+    borrarPorCedula(cedula)
+}
 
 // Metodos de consumo CRUD
-const obtenerClientePorCedula = (cedula) => {
+const obtenerPorCedula = async (cedula) => {
+    const data = axios.get(`http://localhost:8090/api/facturacion/v1/clientes/cedula/${cedula}`).then(r => r.data)
+    console.log(data.nombre)
+    return data
+}
+
+const insertar = async (body) => {
+    axios.put(`http://localhost:8090/api/facturacion/v1/clientes`, body).then(r => r.data)
+}
+
+const actualizar = async (id, body) => {
+    axios.put(`http://localhost:8090/api/facturacion/v1/clientes/actualizar`, body).then(r => r.data)
+}
+
+const buscarTodos = () => {
 
 }
 
-const insertar = (body) => {
+const borrarPorId = (id) => {
+    axios.delete(`http://localhost:8090/api/facturacion/v1/clientes/${id}`).then(r => r.data)
+}
+
+const borrarPorCedula = (cedula) => {
 
 }
 
-const actualizar = (cedula, body) => {
-
-}
+// Medodos que se van a exponer
+// export default obtenerPorCedulaFachada; // Solo se puede exponer un solo metodo
---
+++ src/pages/BuscarPorCedula.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>Buscar pod cedula</h1>
+  <input type="text" v-model="cedula">
+  <button @click="consultarCliente">Buscar</button>
+  <button @click="insertar">Insertar</button>
+</template>
+
+<script >
+import {insertarFachada, obtenerPorCedulaFachada} from "../js/api_facturacion/ProcesarCliente.js"
+export default {
+    data() {
+        return {
+            cedula: ''
+        }
+    },
+    methods: {
+        consultarCliente() {
+            obtenerPorCedulaFachada(this.cedula)
+        },
+        insertar() {
+            const miCliente = {
+                nombre:"Cliente 5",
+                apellido:"Apellido 5",
+                cedula:"005",
+                fechaNacimiento:"2023-01-01T00:00:00",
+                genero:"M",
+                email:"aaa"
+            }
+            console.log(miCliente);
+            insertarFachada(miCliente)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
